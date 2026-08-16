490f7330bb962cec3582eed9ada43c08fadb35ce
payanag2 <payanag@atomicmail.io>
2026-08-16 11:55:13 +0330

    Configure Kotlin JVM toolchain 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,10 @@ android {
     }
 }
 
+kotlin {
+    jvmToolchain(17)
+}
+
 dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2025.01.01")
     implementation(composeBom)
