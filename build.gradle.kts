2e0c1dbcd2d5f37507aabc6aef425ce205bdb8c6
payanag2 <payanag@atomicmail.io>
2026-08-16 11:51:25 +0330

    Fix JVM target compatibility for Android build

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,9 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
+
+allprojects {
+    tasks.withType<JavaCompile>().configureEach {
+        options.release.set(17)
+    }
+}
